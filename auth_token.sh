97efd826b5ea099bd97611d74b04c7a317aaf097
LBF38 <contact.lbf38@gmail.com>
2024-02-22 10:01:05 +0100

    fix(repo): :bug: fix the api endpoint
    for local development, the management api is located at `http://localhost:3001/api`

--- auth_token.sh
+++ auth_token.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-creds=$(echo -n "15aizrn12ecvunt6xskl2:6LdsML7toHMJYxarcKRiYd5Aekp6J4lA" | base64)
+creds=$(echo -n "5s09jnb2ur1ribrczo2v2:gtgncRzBhqzH57fZljCUCte0opPmVJYI" | base64)
 token=$(curl --location \
     --request POST 'http://localhost:3001/oidc/token' \
     --header "Authorization: Basic $creds" \
@@ -12,7 +12,7 @@ printf "\nToken: \n%s\n\n" "$token"
 access_token=$(echo "$token" | jq -r '.access_token')
 printf "\naccess_token: %s\n\n" "$access_token"
 response=$(curl --location \
-    --request GET 'https://default.logto.app/api/users' \
+    --request GET 'http://localhost:3001/api/users' \
     --header "Authorization: Bearer ${access_token}" \
     --verbose)
 echo "$response"
